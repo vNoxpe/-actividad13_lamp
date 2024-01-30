67d61864268a8220c01a15ad766133276a63c3dd
Hugo Calvo Aja <ASR212@iesmhp.local>
2024-01-30 10:21:12 +0100

    cambios

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+
+    <title>Actividad 1.3.1</title>
+
+</head>
+
+<body>
+
+    <?php echo "<h1>Actividad 1.3.1: LAMP Sever. Autor: Tu nombre y apellidos</h1>"; ?>   
+
+</body>
+
+</html>
